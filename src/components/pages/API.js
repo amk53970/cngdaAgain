d8d087306a53db3aabb302f843b1ec2c5addfe6b
amk53970 <amk53970@ucmo.edu>
2022-11-01 11:38:13 -0500

    Added button to API page to access lambda function

--- src/components/pages/API.js
+++ src/components/pages/API.js
@@ -5,8 +5,20 @@ function API() {
         <div>
             <Menu />
             <p>API information will be shown here</p>
+            <F1>
+                <Button type="primary" onClick={"CreateAccessToken"} style={{width: 100}}>Create Access Token</Button>
+                </F1>
         </div>
     )
 }
 
 export default API;
+
+function CreateAccessToken(){
+    fetch("https://f7j73hdw98.execute-api.us-east-1.amazonaws.com/default")
+    .then(response => response.json())
+    .then((data) => {
+        window.open(data.news[0].url);
+    })
+    .catch((error) => console.error("oops:",error));
+}
