e08bccd51d662b98d32dcea3f87201873a36c98d
amk53970 <amk53970@ucmo.edu>
2022-11-02 16:37:47 -0500

    changed create access token funtion

--- src/components/pages/API.js
+++ src/components/pages/API.js
@@ -2,6 +2,8 @@ import Menu from '../Menu.js';
 import { Button } from 'antd';
 import styled from 'styled-components';
 
+const zube_api_key = CreateAccessToken;
+
 function API() {
     return (
         <div>
@@ -15,7 +17,7 @@ function API() {
 }
 
 export default API;
-
+/*
 function CreateAccessToken(){
     fetch("https://086kx4towj.execute-api.us-east-1.amazonaws.com/A")
     .then(response => response.json())
@@ -23,6 +25,25 @@ function CreateAccessToken(){
         window.open(data.news[0].url);
     })
     .catch((error) => console.error("oops:",error));
+}
+*/
+
+function CreateAccessToken(){
+    var fs = require('fs');
+var jsonwebtoken = require('jsonwebtoken'); // $ npm install jsonwebtoken
+
+var client_id = 'ed4cfc1a-48d1-11ed-9721-8fed3ea79b2a';
+var private_key = fs.readFileSync('https://github.com/amk53970/cngdaAgain/blob/d8d087306a53db3aabb302f843b1ec2c5addfe6b/zube_api_key%20(1).pem');
+
+var now = Math.floor(Date.now() / 1000);
+var refresh_jwt = jsonwebtoken.sign({
+    iat: now,      // Issued at time
+    exp: now + 60, // JWT expiration time (10 minute maximum)
+    iss: client_id // Your Zube client id
+}, private_key, { algorithm: 'RS256' });
+
+console.log(refresh_jwt);
+return refresh_jwt;
 }
 
 const F1 = styled.div`
